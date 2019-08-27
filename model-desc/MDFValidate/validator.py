d9fead7828031e849a79f13db8910b9ad5e76038
Mark A. Jensen <mark.jensen@nih.gov>
2019-08-27 14:49:58 -0400

    feat(validate): update the validator
    more meaningful error messages
    can use test_validate.py on command-line
    
     $ python test_validate.py <schema_file> <mdf-file-1.yml> <mdf-file-2.yml>...

--- model-desc/MDFValidate/validator.py
+++ model-desc/MDFValidate/validator.py
@@ -1,7 +1,7 @@
 import sys
 import os
 import option_merge as om
-from jsonschema import validate, ValidationError, SchemaError
+from jsonschema import validate, ValidationError, SchemaError, RefResolutionError
 from jsonschema import Draft6Validator as d6
 import yaml
 # from pdb import set_trace
@@ -17,29 +17,38 @@ class MDFValidator:
     try:
       self.schema = yaml.load(sch_file, Loader=yaml.loader.Loader)
     except RuntimeError:
-      print "Problem with '{fn}' ({e}), skipping".format(fn=sch_filename,e=e)
+      print "YAML error in '{fn}':\n{e}".format(fn=sch_filename,e=e)
+    except Exception:
       raise
+
     if (inst_filenames):
       for inst_filename in inst_filenames:
         try:
           inst_file = open(inst_filename,"r")
         except IOError as e:
-          print "Problem with '{fn}' ({e}), skipping".format(fn=inst_filename,e=e)
+          print "Can't open '{fn}' ({e}), skipping".format(fn=inst_filename,e=e)
         try:
-          self.instance.update(yaml.load(inst_file, Loader=yaml.loader.Loader))
+          inst_yaml=yaml.load(inst_file, Loader=yaml.loader.Loader)
+          self.instance.update(inst_yaml)
         except RuntimeError as e:
-          print "Problem with '{fn}' ({e}), skipping".format(fn=inst_filename,e=e)
+          print "YAML error in '{fn}':\n{e}".format(fn=inst_filename,e=e)
+        except Exception:
+          raise
 
   def validate(self):
     print "Checking schema ====="
     try:
       d6.check_schema(self.schema)
     except SchemaError as se:
+      print se
       return se
     if (self.instance):
       print "Checking instance==="
       try:
         validate(instance=self.instance.as_dict(), schema=self.schema)
+      except RefResolutionError as re:
+        print re
+        return re
       except ValidationError as ve:
         for e in d6(self.schema).iter_errors(self.instance.as_dict()):
           print e;
--- model-desc/test_validate.py
+++ model-desc/test_validate.py
@@ -1,8 +1,18 @@
 from MDFValidate.validator import MDFValidator
+import sys
 
-v = MDFValidator("mdf-schema.yaml", "icdc-model.yml","icdc-model-props.yml")
+test_me = ["mdf-schema.yaml", "icdc-model.yml","icdc-model-props.yml"]
+# v = MDFValidator("mdf-schema.yaml", "icdc-model.yml","icdc-model-props.yml")
+if len(sys.argv) > 1:
+  test_me = sys.argv[1:]
+  
+v = MDFValidator(*test_me)
 
 def test_model_valid():
   assert v.validate() == None
+
+
+if __name__ == '__main__':
+   v.validate()
 
 
